160dec4b17fd06c9ec2e1ed17c17492ab802387b
Bernie White <bewhite@microsoft.com>
2019-11-28 22:48:26 +1000

    Add GitHub and CI files

---
+++ src/PSRule.Monitor/PSRule.Monitor.psm1
@@ -0,0 +1,33 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+#
+# PSRule.Monitor module
+#
+
+Set-StrictMode -Version latest;
+
+[PSRule.Monitor.Configuration.PSRuleOption]::UseExecutionContext($ExecutionContext);
+
+#
+# Localization
+#
+
+#
+# Public functions
+#
+
+#region Public functions
+
+
+#endregion Public functions
+
+#
+# Helper functions
+#
+
+#
+# Export module
+#
+
+# Export-ModuleMember -Function ;
