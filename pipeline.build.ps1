26b5f976087bddb1d8215218a2737d1fe7dc85e2
Bernie White <bewhite@microsoft.com>
2019-11-28 22:50:03 +1000

    Add GitHub and CI files (#2)

---
+++ pipeline.build.ps1
@@ -0,0 +1,282 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $False)]
+    [String]$Build = '0.0.1',
+
+    [Parameter(Mandatory = $False)]
+    [String]$Configuration = 'Debug',
+
+    [Parameter(Mandatory = $False)]
+    [String]$ApiKey,
+
+    [Parameter(Mandatory = $False)]
+    [Switch]$CodeCoverage = $False,
+
+    [Parameter(Mandatory = $False)]
+    [String]$ArtifactPath = (Join-Path -Path $PWD -ChildPath out/modules)
+)
+
+Write-Host -Object "[Pipeline] -- PWD: $PWD" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- ArtifactPath: $ArtifactPath" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- BuildNumber: $($Env:BUILD_BUILDNUMBER)" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- SourceBranch: $($Env:BUILD_SOURCEBRANCH)" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- SourceBranchName: $($Env:BUILD_SOURCEBRANCHNAME)" -ForegroundColor Green;
+
+if ($Env:SYSTEM_DEBUG -eq 'true') {
+    $VerbosePreference = 'Continue';
+}
+
+if ($Env:BUILD_SOURCEBRANCH -like '*/tags/*' -and $Env:BUILD_SOURCEBRANCHNAME -like 'v0.*') {
+    $Build = $Env:BUILD_SOURCEBRANCHNAME.Substring(1);
+}
+
+$version = $Build;
+$versionSuffix = [String]::Empty;
+
+if ($version -like '*-*') {
+    [String[]]$versionParts = $version.Split('-', [System.StringSplitOptions]::RemoveEmptyEntries);
+    $version = $versionParts[0];
+
+    if ($versionParts.Length -eq 2) {
+        $versionSuffix = $versionParts[1];
+    }
+}
+
+Write-Host -Object "[Pipeline] -- Using version: $version" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- Using versionSuffix: $versionSuffix" -ForegroundColor Green;
+
+if ($Env:coverage -eq 'true') {
+    $CodeCoverage = $True;
+}
+
+# Copy the PowerShell modules files to the destination path
+function CopyModuleFiles {
+    param (
+        [Parameter(Mandatory = $True)]
+        [String]$Path,
+
+        [Parameter(Mandatory = $True)]
+        [String]$DestinationPath
+    )
+
+    process {
+        $sourcePath = Resolve-Path -Path $Path;
+
+        Get-ChildItem -Path $sourcePath -Recurse -File -Include *.ps1,*.psm1,*.psd1,*.ps1xml | Where-Object -FilterScript {
+            ($_.FullName -notmatch '(\.(cs|csproj)|(\\|\/)(obj|bin))')
+        } | ForEach-Object -Process {
+            $filePath = $_.FullName.Replace($sourcePath, $destinationPath);
+
+            $parentPath = Split-Path -Path $filePath -Parent;
+
+            if (!(Test-Path -Path $parentPath)) {
+                $Null = New-Item -Path $parentPath -ItemType Directory -Force;
+            }
+
+            Copy-Item -Path $_.FullName -Destination $filePath -Force;
+        };
+    }
+}
+
+task VersionModule ModuleDependencies, {
+    $modulePath = Join-Path -Path $ArtifactPath -ChildPath PSRule.Monitor;
+    $manifestPath = Join-Path -Path $modulePath -ChildPath PSRule.Monitor.psd1;
+    Write-Verbose -Message "[VersionModule] -- Checking module path: $modulePath";
+
+    if (![String]::IsNullOrEmpty($Build)) {
+        # Update module version
+        if (![String]::IsNullOrEmpty($version)) {
+            Write-Verbose -Message "[VersionModule] -- Updating module manifest ModuleVersion";
+            Update-ModuleManifest -Path $manifestPath -ModuleVersion $version;
+        }
+
+        # Update pre-release version
+        if (![String]::IsNullOrEmpty($versionSuffix)) {
+            Write-Verbose -Message "[VersionModule] -- Updating module manifest Prerelease";
+            Update-ModuleManifest -Path $manifestPath -Prerelease $versionSuffix;
+        }
+    }
+
+    $manifest = Test-ModuleManifest -Path $manifestPath;
+    $requiredModules = $manifest.RequiredModules | ForEach-Object -Process {
+        if ($_.Name -eq 'PSRule' -and $Configuration -eq 'Release') {
+            @{ ModuleName = 'PSRule'; ModuleVersion = '0.11.0' }
+        }
+        else {
+            @{ ModuleName = $_.Name; ModuleVersion = $_.Version }
+        }
+    };
+    Update-ModuleManifest -Path $manifestPath -RequiredModules $requiredModules;
+}
+
+# Synopsis: Publish to PowerShell Gallery
+task ReleaseModule VersionModule, {
+    $modulePath = (Join-Path -Path $ArtifactPath -ChildPath PSRule.Monitor);
+    Write-Verbose -Message "[ReleaseModule] -- Checking module path: $modulePath";
+
+    if (!(Test-Path -Path $modulePath)) {
+        Write-Error -Message "[ReleaseModule] -- Module path does not exist";
+    }
+    elseif (![String]::IsNullOrEmpty($ApiKey)) {
+        Publish-Module -Path $modulePath -NuGetApiKey $ApiKey;
+    }
+}
+
+# Synopsis: Install NuGet provider
+task NuGet {
+    if ($Null -eq (Get-PackageProvider -Name NuGet -ErrorAction Ignore)) {
+        Install-PackageProvider -Name NuGet -Force -Scope CurrentUser;
+    }
+}
+
+# Synopsis: Install Pester module
+task Pester NuGet, {
+    if ($Null -eq (Get-InstalledModule -Name Pester -MinimumVersion 4.9.0 -ErrorAction Ignore)) {
+        Install-Module -Name Pester -MinimumVersion 4.9.0 -Scope CurrentUser -Force -SkipPublisherCheck;
+    }
+    Import-Module -Name Pester -Verbose:$False;
+}
+
+# Synopsis: Install PSScriptAnalyzer module
+task PSScriptAnalyzer NuGet, {
+    if ($Null -eq (Get-InstalledModule -Name PSScriptAnalyzer -MinimumVersion 1.18.3 -ErrorAction Ignore)) {
+        Install-Module -Name PSScriptAnalyzer -MinimumVersion 1.18.3 -Scope CurrentUser -Force;
+    }
+    Import-Module -Name PSScriptAnalyzer -Verbose:$False;
+}
+
+# Synopsis: Install PSRule
+task PSRule NuGet, {
+    if ($Null -eq (Get-InstalledModule -Name PSRule -MinimumVersion 0.11.0 -ErrorAction Ignore)) {
+        Install-Module -Name PSRule -Repository PSGallery -MinimumVersion 0.11.0 -Scope CurrentUser -Force;
+    }
+    Import-Module -Name PSRule -Verbose:$False;
+}
+
+# Synopsis: Install PSDocs
+task PSDocs NuGet, {
+    if ($Null -eq (Get-InstalledModule -Name PSDocs -MinimumVersion 0.6.3 -ErrorAction Ignore)) {
+        Install-Module -Name PSDocs -Repository PSGallery -MinimumVersion 0.6.3 -Scope CurrentUser -Force;
+    }
+    Import-Module -Name PSDocs -Verbose:$False;
+}
+
+# Synopsis: Install PlatyPS module
+task platyPS {
+    if ($Null -eq (Get-InstalledModule -Name PlatyPS -MinimumVersion 0.14.0 -ErrorAction Ignore)) {
+        Install-Module -Name PlatyPS -Scope CurrentUser -MinimumVersion 0.14.0 -Force;
+    }
+    Import-Module -Name PlatyPS -Verbose:$False;
+}
+
+# Synopsis: Install module dependencies
+task ModuleDependencies NuGet, PSRule, {
+}
+
+task BuildDotNet {
+    exec {
+        # Build library
+        # Add build version -p:versionPrefix=$ModuleVersion
+        dotnet publish src/PSRule.Monitor -c $Configuration -f netstandard2.0 -o $(Join-Path -Path $PWD -ChildPath out/modules/PSRule.Monitor)
+    }
+}
+
+task TestDotNet {
+    if ($CodeCoverage) {
+        exec {
+            # Test library
+            # dotnet test --collect:"Code Coverage" --logger trx -r (Join-Path $PWD -ChildPath reports/) tests/PSRule.Monitor.Tests
+        }
+    }
+    else {
+        exec {
+            # Test library
+            # dotnet test --logger trx -r (Join-Path $PWD -ChildPath reports/) tests/PSRule.Monitor.Tests
+        }
+    }
+}
+
+task CopyModule {
+    CopyModuleFiles -Path src/PSRule.Monitor -DestinationPath out/modules/PSRule.Monitor;
+}
+
+# Synopsis: Build modules only
+task BuildModule BuildDotNet, CopyModule
+
+task TestRules TestDotNet, PSRule, Pester, PSScriptAnalyzer, {
+    # Run Pester tests
+    $pesterParams = @{ Path = (Join-Path -Path $PWD -ChildPath tests/PSRule.Monitor.Tests); OutputFile = 'reports/pester-unit.xml'; OutputFormat = 'NUnitXml'; PesterOption = @{ IncludeVSCodeMarker = $True }; PassThru = $True; };
+
+    if ($CodeCoverage) {
+        $pesterParams.Add('CodeCoverage', (Join-Path -Path $PWD -ChildPath 'out/modules/**/*.psm1'));
+        $pesterParams.Add('CodeCoverageOutputFile', (Join-Path -Path $PWD -ChildPath reports/pester-coverage.xml));
+    }
+
+    if (!(Test-Path -Path reports)) {
+        $Null = New-Item -Path reports -ItemType Directory -Force;
+    }
+
+    $results = Invoke-Pester @pesterParams;
+
+    # Throw an error if pester tests failed
+    if ($Null -eq $results) {
+        throw 'Failed to get Pester test results.';
+    }
+    elseif ($results.FailedCount -gt 0) {
+        throw "$($results.FailedCount) tests failed.";
+    }
+}
+
+# Synopsis: Run script analyzer
+task Analyze Build, PSScriptAnalyzer, {
+    Invoke-ScriptAnalyzer -Path out/modules/PSRule.Monitor;
+}
+
+# Synopsis: Build table of content for rules
+task BuildRuleDocs Build, PSRule, PSDocs, {
+    Import-Module (Join-Path -Path $PWD -ChildPath out/modules/PSRule.Monitor) -Force;
+    $Null = Invoke-PSDocument -Name Azure -OutputPath .\docs\rules\en-US\ -Path .\RuleToc.Doc.ps1;
+    # $rules = Get-PSRule -Module 'PSRule.Monitor';
+    # $rules | ForEach-Object -Process {
+    #     Invoke-PSDocument -Path .\RuleHelp.Doc.ps1 -OutputPath .\docs\rules\en-US\ -InstanceName $_.Info.Name -inputObject $_;
+    # }
+}
+
+# Synopsis: Build help
+task BuildHelp BuildModule, PlatyPS, {
+    # Generate MAML and about topics
+    # $Null = New-ExternalHelp -OutputPath out/docs/PSRule.Monitor -Path '.\docs\commands\PSRule.Monitor\en-US' -Force;
+
+    # Copy generated help into module out path
+    # $Null = Copy-Item -Path out/docs/PSRule.Monitor/ -Destination out/modules/PSRule.Monitor/en-US/ -Recurse;
+    # $Null = Copy-Item -Path out/docs/PSRule.Monitor/ -Destination out/modules/PSRule.Monitor/en-AU/ -Recurse;
+    # $Null = Copy-Item -Path out/docs/PSRule.Monitor/ -Destination out/modules/PSRule.Monitor/en-GB/ -Recurse;
+}
+
+task ScaffoldHelp Build, BuildRuleDocs, {
+    Import-Module (Join-Path -Path $PWD -ChildPath out/modules/PSRule.Monitor) -Force;
+    Update-MarkdownHelp -Path '.\docs\commands\PSRule.Monitor\en-US';
+}
+
+# Synopsis: Add shipit build tag
+task TagBuild {
+    if ($Null -ne $Env:BUILD_DEFINITIONNAME) {
+        Write-Host "`#`#vso[build.addbuildtag]shipit";
+    }
+}
+
+# Synopsis: Remove temp files.
+task Clean {
+    Remove-Item -Path out,reports -Recurse -Force -ErrorAction SilentlyContinue;
+}
+
+task Build Clean, BuildModule, VersionModule, BuildHelp
+
+task Test Build, TestRules
+
+task Release ReleaseModule, TagBuild
+
+task . Build, Test
